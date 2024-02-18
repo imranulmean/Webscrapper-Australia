c0f381a7ecc0a69f23c06c29d51b811dcfd71d3c
Imranul Hasan <imranulhasan73@gmail.com>
2024-02-18 16:58:07 +0600

    update

--- woolworths2.com.js
+++ woolworths2.com.js
@@ -4,35 +4,64 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 const mainDomain = "https://www.woolworths.com.au/";
-const paginationURLRoot = "shop/browse/dairy-eggs-fridge";
-// document.querySelector("wc-product-tile").shadowRoot.querySelector(".title").innerHTML
+const visitedURLs = [];
+const paginationQueueURLsToVisit = ["shop/browse/dairy-eggs-fridge"];
+const maxPages = 100;
+const products=[];
+//document.querySelector("wc-product-tile").shadowRoot.querySelector(".title").innerHTML
 const test= async () =>{ 
     // const pageHTML1 = await axios.get('https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge?pageNumber=2');
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.setViewport({width: 1080, height: 1024});
-    await page.goto(`https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge?pageNumber=2`, { waitUntil: 'domcontentloaded' });
-    // Wait for the wc-product-tile element to be present
-    await page.waitForSelector('wc-product-tile', { visible: true, timeout: 60000 });
-    page.on('console', message => console.log(message.text()));
-    // Evaluate within the browser context to get the product titles
-    const productTitles = await page.$$eval('wc-product-tile', (wcProductTiles) => {
-      return wcProductTiles.map((wcProductTile) => {
-        const shadowRoot = wcProductTile.shadowRoot;
-        const titleElement = shadowRoot.querySelector('.title a');
-        return titleElement ? titleElement.innerHTML : null;
-      });
-    });
-  
-    // Log the product titles
-    console.log('Product Titles:', productTitles);
 
-    const navigationLinks = await page.$$eval('.paging-section a', (pages) => {
-      return pages.map((page) => page.getAttribute('href'));
-    });    
-    console.log(navigationLinks);
-  
-    await browser.close();
+    while(paginationQueueURLsToVisit.length !== 0 && visitedURLs.length <= maxPages){
+      let onPageProducts=[];
+      const paginationURLRoot = paginationQueueURLsToVisit.pop();
+      if(!paginationURLRoot || paginationURLRoot==='undefined' || paginationURLRoot===undefined){
+        break;
+      }
+      visitedURLs.push(paginationURLRoot);
+
+      await page.goto(`${mainDomain}${paginationURLRoot}`, { waitUntil: 'domcontentloaded' });
+      await page.waitForSelector('wc-product-tile', { visible: true, timeout: 2 * 60000 });
+      onPageProducts = await page.evaluate(() => {
+        const onPageProducts = [];
+        const shadowRoots=[];
+        const wcProductTiles = document.querySelectorAll('wc-product-tile');      
+        wcProductTiles.forEach((wcProductTile) => {
+          const shadowRoot = wcProductTile.shadowRoot;
+          const titleElement = shadowRoot.querySelector('.title a');
+          const title = titleElement ? titleElement.textContent.trim() : null;
+    // Extracting price logic
+          const priceElement = shadowRoot.querySelector('.product-tile-price .primary');
+          const productPrice = priceElement ? priceElement.textContent.trim() : null;
+          const productUrl= titleElement.getAttribute('href')
+          if(productPrice!==null)
+            onPageProducts.push({ title, productPrice, productUrl });
+          
+        });
+        return onPageProducts;
+      });      
+      console.log('onPageProducts:', onPageProducts);
+      let paginationObj={
+        "paginationUrl":`${mainDomain}${paginationURLRoot}`
+      };
+      paginationObj['onPageProducts']=onPageProducts;
+      products.push(paginationObj);
+      const navigationLinks = await page.$$eval('.paging-section a', (pages) => {
+        return pages.map((page) => page.getAttribute('href'));
+      });
+      // console.log(navigationLinks);
+      navigationLinks.map(paginationPageUrl=>{
+        if(!visitedURLs.includes(paginationPageUrl) && !paginationQueueURLsToVisit.includes(paginationPageUrl)){
+          paginationQueueURLsToVisit.push(paginationPageUrl)
+        }
+      })
+    }
+    await browser.close();    
+    const productsString = JSON.stringify(products, null, 2);
+    fs.writeFileSync('./WoolsProducts.json', productsString);
 }
 test();
 
